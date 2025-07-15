bc3c1574c6df65ed06fb3432bd59a69ad716ad3a
Ozodbek-8486 <ozodbekusmonqulov7777@gmail.com>
2025-07-15 08:57:33 +0500

    Initial commit

---
+++ python.py
@@ -0,0 +1,11 @@
+Foydalanuvchidan 3 ta son kiriting. Dastur ushbu sonlar asosida quyidagilarni aniqlasin:
+
+Agar hammasi musbat bo‘lsa → "Barcha sonlar musbat"
+
+Agar faqat bittasi manfiy bo‘lsa → "Faqat bitta son manfiy"
+
+Agar ikki tasi manfiy bo‘lsa → "Ikki son manfiy"
+
+Agar hammasi manfiy bo‘lsa → "Barcha sonlar manfiy"
+
+Aks holda → "Nol qatnashgan"
