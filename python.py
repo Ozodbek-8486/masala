8e8987cba8b92f63ff7a498c940900796f831003
Ozodbek-8486 <ozodbekusmonqulov7777@gmail.com>
2025-07-15 09:58:12 +0500

    f

--- python.py
+++ python.py
@@ -9,3 +9,17 @@
 # Agar baho 2 bo‘lsa → “Qoniqarsiz”
 
 # Aks holda → “Noto‘g‘ri baho kiritildi”
+
+# 2-masala
+
+# Foydalanuvchidan yoshini kirtishini so'rang va kiritilgan yoshga qarab quyidagi xabarlarni chiqaring:
+# Agar foydalanuvchi 18 yoshdan katta bo'lsa → “Sizga kirish narxi 10000 so'm”
+# Agar foydalanuvchi 18 yoshda bo'lsa → “Sizga kirish narxi 5000 so'm”
+# Agar foydalanuvchi 18 yoshdan kichik bo'lsa → “Sizga kirish narxi bepul”
+
+
+# 3-masala
+# Foydalanuvchidan son kiritishini sorang va kiritilgan sonning musbat yoki manfiyligini aniqlang.
+# Agar son musbat bo'lsa → “Musbat son”
+# Agar son manfiy bo'lsa → “Manfiy son”
+# Agar foydalanuvchi 0 kiritgan bo'lsa → “Bu nol”
